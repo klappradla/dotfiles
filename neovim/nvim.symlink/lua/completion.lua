659fd68bd0471e81e8a72c9dc74bebabd3de1d3a
Max Mulatz <klappradla@posteo.net>
2022-11-29 00:29:23 +0100

    Replace snippet engine in cmp with DIY solution
    This is directly copied over from the nvim-cmp wiki and only a temporary
    solution to get rid of untisnips.

--- neovim/nvim.symlink/lua/completion.lua
+++ neovim/nvim.symlink/lua/completion.lua
@@ -4,11 +4,30 @@ local get_all_buffers = function()
   return vim.api.nvim_list_bufs()
 end
 
+-- simple snippet plugin implementation from
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#no-snippet-plugin
+local expand_snippet = function(args)
+	local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+	local indent = string.match(line_text, "^%s*")
+	local replace = vim.split(args.body, "\n", true)
+	local surround = string.match(line_text, "%S.*") or ""
+	local surround_end = surround:sub(col)
+
+	replace[1] = surround:sub(0, col - 1) .. replace[1]
+	replace[#replace] = replace[#replace] .. (#surround_end > 1 and " " or "") .. surround_end
+	if indent ~= "" then
+		for i, line in ipairs(replace) do
+			replace[i] = indent .. line
+		end
+	end
+
+	vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+end
+
 cmp.setup({
 	snippet = {
-		expand = function(args)
-			vim.fn["UltiSnips#Anon"](args.body)
-		end,
+		expand = expand_snippet,
 	},
 	window = {
 		completion = cmp.config.window.bordered(),
