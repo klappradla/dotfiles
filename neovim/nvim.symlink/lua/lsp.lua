86098687b1bdc03bbd443dd93164aca49636b75d
Max Mulatz <klappradla@posteo.net>
2022-11-30 22:44:34 +0100

    Add .stylua file and update lua formatting
    Use spaces instead of tabs and only indent 2 levels deep. I find it hard
    to read otherwise.

--- neovim/nvim.symlink/lua/lsp.lua
+++ neovim/nvim.symlink/lua/lsp.lua
@@ -6,28 +6,28 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "<leader>D", vim.diagnostic.setloclist, opts)
 
 local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			return client.name == "null-ls" -- use only null-ls for formatting
-		end,
-		bufnr = bufnr,
-		async = true,
-	})
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls" -- use only null-ls for formatting
+    end,
+    bufnr = bufnr,
+    async = true,
+  })
 end
 
 local on_attach = function(_client, bufnr)
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-	-- Mappings
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "<leader>fm", function()
-		lsp_formatting(bufnr)
-	end, bufopts)
+  -- Mappings
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<leader>fm", function()
+    lsp_formatting(bufnr)
+  end, bufopts)
 end
 
 -- Language servers
 lspconfig.tsserver.setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 return { on_attach = on_attach }
